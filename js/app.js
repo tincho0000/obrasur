edb8ffc248db410b6d613ff1e1e7141ec2868ca7
tincho0000 <tincho0000@gmail.com>
2018-12-03 00:38:15 -0300

    Mejorando menu y cambiando tamaños segun dispositivos

--- js/app.js
+++ js/app.js
@@ -2,10 +2,13 @@ window.onscroll = function(e) {
     let scroll = window.scrollY;
 
     const headerScroll = document.querySelector('#navegacion-principal');
-    console.log(scroll);
+    const dropdownScroll = document.querySelector('#dropdown-menu');
+
     if (scroll > 300) {
         headerScroll.classList.add('navegacion-principal');
+        dropdownScroll.classList.add('navegacion-principal');
     } else {
         headerScroll.classList.remove('navegacion-principal');
+        dropdownScroll.classList.remove('navegacion-principal');
     }
 };
