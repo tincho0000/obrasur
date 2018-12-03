1e025ede77c8e5d2dc1c5ecd0b710ec8e092a46f
tincho0000 <tincho0000@gmail.com>
2018-12-03 11:20:27 -0300

    Se agrega fondo a los dropdown del navbar

--- js/app.js
+++ js/app.js
@@ -2,13 +2,16 @@ window.onscroll = function(e) {
     let scroll = window.scrollY;
 
     const headerScroll = document.querySelector('#navegacion-principal');
-    const dropdownScroll = document.querySelector('#dropdown-menu');
+    const nosotrosScroll = document.querySelector('#dropdown-nosotros');
+    const proyectoScroll = document.querySelector('#dropdown-proyecto');
 
     if (scroll > 300) {
         headerScroll.classList.add('navegacion-principal');
-        dropdownScroll.classList.add('navegacion-principal');
+        nosotrosScroll.classList.add('navegacion-principal');
+        proyectoScroll.classList.add('navegacion-principal');
     } else {
         headerScroll.classList.remove('navegacion-principal');
-        dropdownScroll.classList.remove('navegacion-principal');
+        nosotrosScroll.classList.remove('navegacion-principal');
+        proyectoScroll.classList.remove('navegacion-principal');
     }
 };
